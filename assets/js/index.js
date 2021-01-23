9755f3993a27bf8cd98817aa6ed480cb3ea2b946
Count Zero <joelecox1@gmail.com>
2021-01-22 20:06:59 -0600

    centered save icon & added screenshot

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,12 +11,11 @@ function time() {
   var currentHour = moment().hours();
 
   $(".time-block").each(function() {
-    var rowHour = $(this).attr("id");
+    var rowHour = parseInt($(this).attr("id"));
 
     if (rowHour < currentHour) {
       $(this).addClass("past");
     } else if (rowHour === currentHour) {
-      console.log(this);
       $(this).removeClass("past");
       $(this).addClass("present");
     } else {
@@ -30,14 +29,14 @@ function time() {
 time();
 
 var interval = setInterval(time, 15000);
-$("9 .description").val(localStorage.getItem("9"));
-$("10 .description").val(localStorage.getItem("10"));
-$("11 .description").val(localStorage.getItem("11"));
-$("12 .description").val(localStorage.getItem("12"));
-$("13 .description").val(localStorage.getItem("13"));
-$("14 .description").val(localStorage.getItem("14"));
-$("15 .description").val(localStorage.getItem("15"));
-$("16 .description").val(localStorage.getItem("16"));
-$("17 .description").val(localStorage.getItem("17"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
